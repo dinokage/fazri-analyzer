20e40d641898878f59f79fc45392002c5cda6ee3
Edantuti <edantuti@gmail.com>
2025-10-03 23:43:28 +0530

    refactored code

--- src/components/ui/auth/login-button.tsx
+++ src/components/ui/auth/login-button.tsx
@@ -2,14 +2,12 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 
-export default function LoginButton(){
-  const signInWithGoogle = async ()=>{
+export default function LoginButton() {
+  const signInWithGoogle = async () => {
     await authClient.signIn.social({
-      provider:"google",
-      callbackURL:"/"
-    })
-  }
-    return(
-        <Button onClick={signInWithGoogle}>Login</Button>
-    )
+      provider: "google",
+      callbackURL: "/",
+    });
+  };
+  return <Button onClick={signInWithGoogle}>Login</Button>;
 }
