d94191be720cf2267b04be8ceec94e6b280e0a7c
Edantuti <edantuti@gmail.com>
2025-10-03 23:43:28 +0530

    refactored code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import LoginButton from "@/components/ui/auth/login-button";
 import LogOutButton from "@/components/ui/auth/logout-button";
-import { auth } from "@/lib/auth"
-import { headers } from "next/headers"
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 export default async function Home() {
   const session = await auth.api.getSession({
-        headers: await headers()
-    })
+    headers: await headers(),
+  });
   return (
     <div>
       <h1>Home</h1>
-      {!session ? <LoginButton /> : <div>
-          <h2>we present you our website. Mr. {session.user.name}</h2>       
+      {!session ? (
+        <LoginButton />
+      ) : (
+        <div>
+          <h2>we present you our website. Mr. {session.user.name}</h2>
           <LogOutButton />
-        </div>}
+        </div>
+      )}
     </div>
   );
 }
